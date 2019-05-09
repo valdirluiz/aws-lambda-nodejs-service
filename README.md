f4f28d752a07e9b4693d02de266b139081d4bd7c
Valdir <valdir@substractum.com.br>
2019-05-09 10:52:21 -0300

    implementar api

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 https://serverless.com/blog/node-rest-api-with-serverless-lambda-and-dynamodb/
+
+
+curl -H "Content-Type: application/json" -X POST -d '{"fullname":"Shekhar Gulati","email": "shekhargulati84@gmail.com", "experience":12}' https://0wvrn8uc6e.execute-api.us-east-1.amazonaws.com/dev/candidates
--- api/candidate.js
+++ api/candidate.js
@@ -1,12 +1,75 @@
 'use strict';
 
+
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+
+
+AWS.config.setPromisesDependency(require('bluebird'));
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
 module.exports.submit = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+
+  const requestBody = JSON.parse(event.body);
+  const fullname = requestBody.fullname;
+  const email = requestBody.email;
+  const experience = requestBody.experience;
+
+
+  if (typeof fullname !== 'string' || typeof email !== 'string' || typeof experience !== 'number') {
+    console.error('Validation Failed');
+    callback(new Error('Couldn\'t submit candidate because of validation errors.'));
+    return;
+  }
+
+
+  submitCandidateP(candidateInfo(fullname, email, experience))
+    .then(res => {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: `Sucessfully submitted candidate with email ${email}`,
+          candidateId: res.id
+        })
+
+      });
+
+    }).catch(err => {
+      console.log(err);
+      callback(null, {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: `Unable to submit candidate with email ${email}`
+        })
+      })
+
+    });
+
+};
+
+
+
+const submitCandidateP = candidate => {
+  console.log('Submitting candidate');
+  const candidateInfo = {
+    TableName: process.env.CANDIDATE_TABLE,
+    Item: candidate,
   };
-  callback(null, response);
+
+  return dynamoDb.put(candidateInfo).promise()
+    .then(res => candidate);
+};
+
+
+const candidateInfo = (fullname, email, experience) => {
+  const timestamp = new Date().getTime();
+  return {
+    id: uuid.v1(),
+    fullname: fullname,
+    email: email,
+    experience: experience,
+    submittedAt: timestamp,
+    updatedAt: timestamp,
+  };
 };
